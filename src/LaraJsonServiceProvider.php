c01700b51f6c889ee168c1127ae93670c0a0aada
Salih Baş <salih.bas@imgesoft.com.tr>
2022-07-18 18:22:06 +0300

    v1.1.3

---
+++ src/LaraJsonServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Salih\Composer;
+
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+
+class LaraJsonServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if (! $this->app->runningInConsole()) {
+            return;
+        }
+
+        $this->commands([
+            Console\TestCommand::class,
+        ]);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [Console\TestCommand::class];
+    }
+}
